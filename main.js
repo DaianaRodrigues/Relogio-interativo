61aa03a65082cda7b488614e4918b0db6b105ce7
Daiana Rodrigues Nascimento <102737592+DaianaRodrigues@users.noreply.github.com>
2022-08-20 20:17:37 -0300

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function carregar() {
     msg.innerHTML = `Agora são ${hora} horas e ${minutos} minutos`;
     if (hora >= 0 && hora < 12) {
         //Bom dia
-        img.src = 'Slide1.jpg';
+        img.src = 'fotomanha.png';
         document.body.style.background = '#e2cd9f';
     } else if (hora >= 12 && hora < 18) {
         //Boa tarde
